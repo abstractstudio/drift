7140fe18de3cf41295166ecb5c1d1e602c705013
Noah Kim <noahbkim@gmail.com>
2016-04-29 15:19:02 -0400

    Started working on the sprite stuff.

--- drift.js
+++ drift.js
@@ -1,9 +1,15 @@
+var MAX_SPEED = 5;
+
 function Boat() {
 	
 	this.x = 300;
 	this.y = 600;
-	this.a = 0;
+	this.a = Math.PI / 2;
 	this.r = 0;
+
+	this.update = function(delta) {
+		
+	}
 
 	this.render = function(context) {
 		context.fillRect(this.x, this.y, 10, 10);
@@ -14,7 +20,8 @@ function Boat() {
 function Drift(canvas) {
 	
 	Engine.call(this, canvas);
-	this.sprites = {"boat": new Boat()}
+	this.sprites = {"boat": new Boat()}; 
+
 }
 
 Boat.prototype = new Sprite();
